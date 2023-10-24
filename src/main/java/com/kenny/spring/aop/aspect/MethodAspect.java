6c6d359f1dc82a62e20750b053ac7ba566b83da5
kenny <kenny.kwyiu@gmail.com>
2023-10-24 13:00:21 +0800

    24 Oct 2023 finished before, after, after returning, after throwing aspect method

--- src/main/java/com/kenny/spring/aop/aspect/MethodAspect.java
+++ src/main/java/com/kenny/spring/aop/aspect/MethodAspect.java
@@ -12,6 +12,23 @@ public class MethodAspect {
         String className = joinPoint.getTarget().getClass().getName();// get the target class name
         String methodName = joinPoint.getSignature().getName(); // get the target method name
         System.out.println("---->" + now + ":" + className + "." + methodName);
-
+        Object[] args = joinPoint.getArgs();
+        System.out.println("---->numbers of args: " + args.length);
+        for (Object arg : args) {
+            System.out.println("---->agrs: " + arg);
+        }
     }
+
+    public void doAfterReturning(JoinPoint joinPoint, Object returning) {
+        System.out.println("<----returning advice: " + returning);
+    }
+
+    public void doAfterThrowing(JoinPoint joinPoint, Throwable throwable) {
+        System.out.println("<----error inform: " + throwable.getMessage());
+    }
+
+    public void doAfter(JoinPoint joinPoint) {
+        System.out.println("<----trigger doAfter Advice");
+    }
+
 }
